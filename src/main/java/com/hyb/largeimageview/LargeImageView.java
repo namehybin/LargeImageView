87d51d7bbcee69aa08449ba06f7dd69515e58cee
youbinhuang <youbinhuang@meilishuo.cm>
2016-03-27 23:28:23 +0800

    init

--- src/main/java/com/hyb/largeimageview/LargeImageView.java
+++ src/main/java/com/hyb/largeimageview/LargeImageView.java
@@ -1,18 +1,160 @@
 package com.hyb.largeimageview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Huangyoubin on 16/3/27.
  */
 public class LargeImageView extends View {
+
+    private BitmapRegionDecoder mDecoder;
+    /**
+     * 图片的宽度和高度
+     */
+    private int mImageWidth, mImageHeight;
+    /**
+     * 绘制的区域
+     */
+    private volatile Rect mRect = new Rect();
+
+    private final BitmapFactory.Options options = new BitmapFactory.Options();
+
+    {
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+    }
+
+
     public LargeImageView(Context context) {
         super(context);
     }
 
     public LargeImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    public void setInputStream(InputStream is) {
+        try {
+            mDecoder = BitmapRegionDecoder.newInstance(is, false);
+            BitmapFactory.Options tmpOptions = new BitmapFactory.Options();
+            tmpOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(is, null, tmpOptions);
+            mImageWidth = tmpOptions.outWidth;
+            mImageHeight = tmpOptions.outHeight;
+
+            requestLayout();
+            invalidate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    private void checkWidth() {
+        Rect rect = mRect;
+        int imageWidth = mImageWidth;
+
+        if (rect.right > imageWidth) {
+            rect.right = imageWidth;
+            rect.left = imageWidth - getWidth();
+        }
+
+        if (rect.left < 0) {
+            rect.left = 0;
+            rect.right = getWidth();
+        }
+    }
+
+
+    private void checkHeight() {
+
+        Rect rect = mRect;
+        int imageHeight = mImageHeight;
+
+        if (rect.bottom > imageHeight) {
+            rect.bottom = imageHeight;
+            rect.top = imageHeight - getHeight();
+        }
+
+        if (rect.top < 0) {
+            rect.top = 0;
+            rect.bottom = getHeight();
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Bitmap bm = mDecoder.decodeRegion(mRect, options);
+        canvas.drawBitmap(bm, 0, 0, null);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+
+        int imageWidth = mImageWidth;
+        int imageHeight = mImageHeight;
+
+        //默认直接显示图片的中心区域，可以自己去调节
+        mRect.left = imageWidth / 2 - width / 2;
+        mRect.top = imageHeight / 2 - height / 2;
+        mRect.right = mRect.left + width;
+        mRect.bottom = mRect.top + height;
+
+    }
+
+    private float preX, preY;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int actionCode = event.getAction();
+        switch (actionCode) {
+            case MotionEvent.ACTION_DOWN:
+                preX = event.getX();
+                preY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int moveX = (int) (event.getX() - preX);
+                int moveY = (int) (event.getY() - preY);
+                preX = event.getX();
+                preY = event.getY();
+                if (mImageWidth > getWidth()) {
+                    mRect.offset(-moveX, 0);
+                    checkWidth();
+                    invalidate();
+                }
+                if (mImageHeight > getHeight()) {
+                    mRect.offset(0, -moveY);
+                    checkHeight();
+                    invalidate();
+                }
+
+                return true;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                break;
+
+        }
+        return true;
     }
 }
--- src/main/java/com/hyb/largeimageview/MainActivity.java
+++ src/main/java/com/hyb/largeimageview/MainActivity.java
@@ -3,11 +3,26 @@ package com.hyb.largeimageview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
+    private LargeImageView largeImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        largeImageView= (LargeImageView) findViewById(R.id.large_image_view);
+        try
+        {
+            InputStream inputStream = getAssets().open("big_image.jpg");
+            largeImageView.setInputStream(inputStream);
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
