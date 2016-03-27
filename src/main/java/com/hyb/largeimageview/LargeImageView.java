c79f937916ddb49b48f54b44bb23937cccaecd10
youbinhuang <youbinhuang@meilishuo.cm>
2016-03-27 11:16:54 +0800

    first commit

---
+++ src/main/java/com/hyb/largeimageview/LargeImageView.java
@@ -0,0 +1,18 @@
+package com.hyb.largeimageview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Huangyoubin on 16/3/27.
+ */
+public class LargeImageView extends View {
+    public LargeImageView(Context context) {
+        super(context);
+    }
+
+    public LargeImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
